0ba53560b23e35b3716285f9516c70423bf52ef2
tania <tte@ciklum.com>
2018-10-14 02:14:16 +0300

    Create project. Move gradle dependencies to config. Add SelectGateWay UI

---
+++ app/src/main/java/com/tesliukdev/fairgame/screens/setup/SelectGateWayActivity.kt
@@ -0,0 +1,34 @@
+package com.tesliukdev.fairgame.screens.setup
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+import com.tesliukdev.fairgame.R
+
+import kotlinx.android.synthetic.main.activity_select_gate_way.*
+
+class SelectGateWayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_gate_way)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
---
+++ app/src/main/java/com/tesliukdev/fairgame/screens/setup/SelectGateWayFragment.kt
@@ -0,0 +1,19 @@
+package com.tesliukdev.fairgame.screens.setup
+
+import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tesliukdev.fairgame.R
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+class SelectGateWayFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_activity_select_gate_way, container, false)
+    }
+}
